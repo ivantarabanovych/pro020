da2bd30e202e0fe0ee1b7792e28b36d63dde78ef
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-16 23:20:45 +0300

    q4

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ class Employee{
     calculateSalary(){
         return this.baseSalary;
     }
+
+    work(){
+        return `${this.name}, виконує свої обов'язки на посаді ${this.position}`;
+    }
 }
 
 class Developer extends Employee{
@@ -18,8 +22,12 @@ class Developer extends Employee{
     }
 
     calculateSalary(){
-        const bonus = 500;
-        return this.baseSalary + bonus;
+        const codeBonus = 500;
+        return this.baseSalary + codeBonus;
+    }
+
+    work(){
+        return `${this.name}, виконує свої обов'язки на посаді ${this.position}, та отримує додатковий бонус за знання мови ${this.programmingLanguage}`;
     }
 }
 
@@ -33,8 +41,22 @@ class Manager extends Employee{
         const bonusPerTeamSize = 100;
         return this.baseSalary + (this.teamSize * 100)
     }
+
+    work(){
+        return `${this.name}, виконує свої обов'язки на посаді ${this.position}, та керує командою яка складається з ${this.teamSize} осіб`;
+    }
 }
 
-const manager = new Manager("Ivan", 21, "manager", 1500, 25);
+const employee = new Employee("Ivan", 21, "sys.Admin", 1500);
+const developer = new Developer("Oleh", 21, "web developer", 1500, "C++", 500);
+const manager = new Manager("Maria", 21, "manager", 1000, 8);
 
-console.log(`${manager.name} earn from month: $${manager.calculateSalary()}`);
+console.log(`${employee.name} Загальна заробітня плата: $${employee.calculateSalary()}`);
+console.log(`${developer.name} Загальна заробітня плата: $${developer.calculateSalary()}`);
+console.log(`${manager.name} Загальна заробітня плата: $${manager.calculateSalary()}`);
+
+console.log(employee.work());
+console.log(developer.work());
+console.log(manager.work());
+
+
